5fc1ddfd6e80b522fab75129d29ec4b374db7170
MAIDENMI <aidenmiah3@gmail.com>
2024-02-10 00:37:29 -0500

    Model dropdown addition

--- client/src/components/ModelDrop.js
+++ client/src/components/ModelDrop.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ModelDrop = () => {
+    const [selectedValue, setSelectedValue] = useState('');
+
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+    }
+    
     return (
         <div>
             Model Drop
+            <div>
+                <select value={selectedValue} onChange={handleChange}>
+                    <option value="">Select an option</option>
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                    // Add more options here
+                </select>
+                {selectedValue && <div>You selected: {selectedValue}</div>}
+            </div>
+
         </div>
     );
 }
