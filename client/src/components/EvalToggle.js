21f2ebcebccaca6b3d868c267556fb67d45d37f8
Major <107882188+qubitflip@users.noreply.github.com>
2024-02-10 20:15:28 -0500

    Removed toggle weirdness

--- client/src/components/EvalToggle.js
+++ client/src/components/EvalToggle.js
@@ -4,10 +4,7 @@ const EvalToggle = () => {
     return (
         <div className="eval-comps">
             <div className="eval-text">Eval Toggle</div>
-            <label className="switch">
-                <input type="checkbox" />
-                <span className="slider round"></span>
-            </label>
+            <input className="eval-toggle" type="checkbox" />
         </div>
     );
 }
